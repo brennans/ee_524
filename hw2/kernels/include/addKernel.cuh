8d1f92b625a7542167cd14546e0410a7e619812c
Brennan Swanton <brennan.swanton@gmail.com>
2022-10-23 16:12:28 -0700

    Initial hw2 commit

---
+++ hw2/kernels/include/addKernel.cuh
@@ -0,0 +1,6 @@
+#ifndef EE_524_ADDKERNEL_CUH
+#define EE_524_ADDKERNEL_CUH
+
+__global__ void addKernel(int *c, const int *a, const int *b);
+
+#endif //EE_524_ADDKERNEL_CUH
---
+++ hw2/kernels/source/addKernel.cu
@@ -0,0 +1,7 @@
+//
+//
+__global__ void addKernel(int *c, const int *a, const int *b)
+{
+    int i = threadIdx.x;
+    c[i] = a[i] + b[i];
+}
